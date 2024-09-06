e2789b23b050c00dfed1f780f39878e0dca0a07b
robloxianstudio123 <148556463+robloxianstudio123@users.noreply.github.com>
2024-09-06 07:13:31 +0200

    Car!!

---
+++ car.lua
@@ -0,0 +1,32 @@
+--[[
+Hello my name is car and I made this song for everyone! Enjoy is please hahehe! 
+
+Open all your eyes. 
+See that my name... is 
+Car, car, car! 
+Tell me, you got it, right? 
+Let me say it one more time! 
+CAR 
+
+I once met a man. 
+His name starts with a G! 
+Thanks to him... 
+I now have... 
+47 friends... 
+and you're one! 
+
+I'll always be here. Forever free. 
+Look ahead... 
+Grow with me! 
+
+Open all your eyes. 
+See that my name... is 
+Car, car, car! 
+Tell me, you got it, right? 
+Let me say it one more time! 
+CAR 
+
+
+
+]]
+getgenv().CarFronGarn47=nil
